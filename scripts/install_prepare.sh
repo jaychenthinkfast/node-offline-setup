c276ab859c1e86c905122e4438d05044eb64ac6f
chenjie <chenjie19@jd.com>
2016-10-25 17:14:04 +0800

    rm tar view

--- scripts/install_prepare.sh
+++ scripts/install_prepare.sh
@@ -2,7 +2,7 @@
 # 准备
 
 cd /data/node_offline_setup/src/
-tar zxvf node-v6.2.0-linux-x64.tar.gz
+tar zxf node-v6.2.0-linux-x64.tar.gz
 mv node-v6.2.0-linux-x64  ../node
-tar zxvf pm2-1.0.0.tar.gz
+tar zxf pm2-1.0.0.tar.gz
 mv pm2  ../npm_modules_global/node_modules/
